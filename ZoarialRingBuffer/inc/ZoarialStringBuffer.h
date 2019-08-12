648ea0fc63d8609965df5768a8dfda57b4444e56
Zoarial94 <hunterw9469@gmail.com>
2019-08-12 15:13:40 -0400

    Added the base functions of the class

--- ZoarialRingBuffer/inc/ZoarialStringBuffer.h
+++ ZoarialRingBuffer/inc/ZoarialStringBuffer.h
@@ -1,8 +1,22 @@
 #pragma once
 
+#include <cstdint>
+#include <string>
+
 class ZoarialStringBuffer {
 private:
 
+	const uint32_t LENGTH;
+	char* const buf;
+
+	uint32_t _start = 0;
+	uint32_t _end = 0;
+
 public:
+
+	ZoarialStringBuffer(const uint32_t len);
+
+	void addString(const std::string& str);
+	std::string getString();
 
 };
---
+++ ZoarialRingBuffer/src/ZoarialStringBuffer.cpp
@@ -0,0 +1,9 @@
+#include "ZoarialStringBuffer.h"
+
+ZoarialStringBuffer::ZoarialStringBuffer(const uint32_t len) : LENGTH(len), buf(new char[len]) {
+
+}
+
+void ZoarialStringBuffer::addString(const std::string& str) {
+
+}
